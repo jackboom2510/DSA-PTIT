b14b4cd7e94d689deb55252c8dbbf3f490d80b21
jackboom2510 <58967934+jackboom2510@users.noreply.github.com>
2025-02-19 08:23:14 +0700

    dsa1-19-2

---
+++ dsa1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int S = 0, n;
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		S += (2*i+1)/(2*i+2);
+	}
+	cout << S << endl;
+	return 0;
+}
